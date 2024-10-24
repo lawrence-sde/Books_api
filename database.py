e075c84f3eda3c92f7305979d597f45cefff64c2
Sujay Lawrence J <sujay.lawrence@augmento.com>
2024-10-24 16:42:20 +0530

    Books and Auth

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+
+DATABASE_URL ="sqlite:///books.db"
+
+engine = create_engine(DATABASE_URL)
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
